4b7bfabeb8286d27505f6ea4eec329253975c772
Ishwari <ishwarinanaware27@gmail.com>
2026-07-20 22:32:56 +0530

    this is pattern 11 of printing equilateral triangle

---
+++ Pattern11.java
@@ -0,0 +1,29 @@
+class Pattern11
+{
+     public static void main(String args[])
+     {
+        fun(5);
+     }
+    public static void fun(int n)
+    {
+        
+        for(int i=0;i<n;i++)
+         {
+            for(int j=0;j<=i;j++)
+		{
+		   System.out.print("*");
+		}
+	     System.out.println();
+	  }
+	for(int i=n;i>=0;i--)
+         {
+            for(int j=0;j<=i;j++)
+		{
+		   System.out.print("*");
+		}
+	     System.out.println();
+	  }
+
+          
+    }
+}
