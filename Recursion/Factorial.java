918eb4a7b06fdcbafc2ed33c1d58e534b257a060
Ishwari <ishwarinanaware27@gmail.com>
2026-07-21 12:02:27 +0530

    this is program for factorial of n numbers where i have took fact variable and store the result by calling fact function recursively like fact=n*fact(n-1) where base condition is when n is 0 or 1 return 1

---
+++ Recursion/Factorial.java
@@ -0,0 +1,17 @@
+class Factorial
+{
+    public static void main(String args[])
+   {
+       System.out.println(fact(5));
+   }
+  public static int fact(int n)
+  {
+      int fact;
+      if(n==0 || n==1)
+      {
+         return 1;
+      }
+     fact=n*fact(n-1);
+    return fact;
+  }
+}
