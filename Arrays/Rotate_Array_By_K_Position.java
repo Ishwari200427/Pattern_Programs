ed468c007f892d74cea9ecd9a8ac241871f3730e
Ishwari <ishwarinanaware27@gmail.com>
2026-07-22 18:13:54 +0530

    updated logic for right rotate

--- Arrays/Rotate_Array_By_K_Position.java
+++ Arrays/Rotate_Array_By_K_Position.java
@@ -1,34 +1,71 @@
 import java.util.*;
-class Rotate_Array_By_K_Position
-{
-    public static void main(String args[])
-    {
-        int arr[]={10,20,30,40,50};
-        Rotate(0,2,arr);
+
+class Rotate_Array_By_K_Position {
+
+    public static void main(String args[]) {
+
+        int arr[] = {10, 20, 30, 40, 50};
         System.out.println(Arrays.toString(arr));
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.println("\n1. Rotate Right");
+            System.out.println("2. Rotate Left");
+            System.out.println("3. Exit");
+            System.out.print("Enter Choice: ");
+
+            int dir = sc.nextInt();
+
+            switch (dir) {
+
+                case 1:
+                    RotateRight(2, arr);
+                    System.out.println(Arrays.toString(arr));
+                    break;
+
+                case 2:
+                    RotateLeft(2, arr);
+                    System.out.println(Arrays.toString(arr));
+                    break;
+
+                case 3:
+                    System.exit(0);
+
+                default:
+                    System.out.println("Enter Valid Choice");
+            }
+        }
     }
-    
-  public static void Rotate(int start,int k,int arr[])
-  {
+
+    public static void RotateLeft(int k, int arr[]) {
+
         k = k % arr.length;
 
         Reverse(arr, 0, k - 1);
         Reverse(arr, k, arr.length - 1);
         Reverse(arr, 0, arr.length - 1);
-      
-       
-  }
- public static void Reverse(int arr[],int start,int end)
-{
-    while(start<end)
-    {
-     int temp=arr[start];
-     arr[start]=arr[end];
-     arr[end]=temp;
-     start++;
-     end--;
     }
-     
-}
 
+    public static void RotateRight(int k, int arr[]) {
+
+        k = k % arr.length;
+
+        Reverse(arr, arr.length - k, arr.length - 1);
+        Reverse(arr, 0, arr.length - k - 1);
+        Reverse(arr, 0, arr.length - 1);
+    }
+
+    public static void Reverse(int arr[], int start, int end) {
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
 }
